23f859f1fc14e301580fa09d092447e6d13bd3fc
har3365 <haris15@bih.net.ba>
2021-04-01 10:25:14 +0200

    Day24

---
+++ ex02/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+class App extends Component {
+  setMyStorage = () => {
+    document.cookie = "Year=2021";
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  };
+  getMyStorage = () => {
+    let myCookieData = document.cookie;
+    let myLocalStorageData = localStorage.getItem("Paragon");
+    let mySessionStorageData = sessionStorage.getItem("frontend");
+    return [myCookieData, myLocalStorageData, mySessionStorageData];
+  };
+  render() {
+    return (
+      <div className="App">
+        <button type="button" onClick={this.setMyStorage}>
+          Button Set
+        </button>
+        <button type="button" onClick={this.getMyStorage}>
+          Button Get
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
